4e43cc290492407f8dd0b9dd2077d912b4f24a7a
Darian McKoy <92386738+DarianMcKoy@users.noreply.github.com>
2022-04-18 21:12:12 -0400

    Update followed.java

--- final/followed.java
+++ final/followed.java
@@ -1,8 +1,8 @@
 package Projects;
 
-public abstract class followed {
+public abstract class followed { //This creates a abstract class and allows to connect to followed java 2  
 
-		public void display() { 
-			System.out.println("You will keep your scholarship next semester.");
+		public void display() { //A comment that can be overwritten. Will skip over if users gpa is under the requirement.//
+			System.out.println("You will keep your scholarship next semester."); //Te comment that would be displayed if the user meets the requirement.//
 }
 }
